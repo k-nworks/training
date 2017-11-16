b1374e901993b939f0ffdb171949fbc6219a855b
K INAI <k-inai@K-MacBook-Pro.local>
2017-11-16 19:18:40 +0900

    20171116研修

--- rails_1/tweet_app/config/routes.rb
+++ rails_1/tweet_app/config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
+  get 'posts/:id'  => 'posts#show'
 
   get 'top' => 'home#top'
   get 'about' => 'home#about'
   get '/' => 'home#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
