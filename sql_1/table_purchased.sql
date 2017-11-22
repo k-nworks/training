98a6b96410e794947819c52d164682e8d922733a
K INAI <k-inai@K-MacBook-Pro.local>
2017-11-22 18:53:16 +0900

    MySQLテーブル作成テスト

---
+++ sql_1/table_purchased.sql
@@ -0,0 +1,1 @@
+create table purchases ( id int(10) not null auto_increment, name varchar(32) not null, price int(10), character_name varchar(32) not null, category varchar(32), purchased_at date, primary key(id));
